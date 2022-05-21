5849a70cee80f36e0697995a0a4914b1f57ed6a7
bneveret <eve20003@byui.edu>
2022-05-21 17:24:45 -0600

    conditionals and loops

---
+++ java/Extra.java
@@ -0,0 +1,12 @@
+public class Extra {
+    public static void main (String[] args) {
+    int s = 25;
+    int r = 0;
+    if (s == 25) {
+        r = 3;
+    }
+    for (int i = r; i > 0; i--) {
+        System.out.println(i);
+    }
+}
+}
